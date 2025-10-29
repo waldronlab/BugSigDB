9853dcc1add5d931ed4b7fdf18d6600605edb5f4
Vedmaka <god.vedmaka@gmail.com>
2025-10-29 23:57:56 +0100

    Removes mainlabel from experiments and signatures

--- export_csv.sh
+++ export_csv.sh
@@ -17,7 +17,7 @@ echo "Done"
 echo "Exporting experiments.."
 php extensions/SemanticReports/maintenance/GenerateReport.php \
   -q "[[Category:Experiments]] |?Original page name=Experiment page name |?Related study=Study |?Location of subjects |?Host species |?Body site |?UBERON ID |?Condition |?EFO ID |?Group 0 name |?Group 1 name |?Group 1 definition |?Group 0 sample size |?Group 1 sample size |?Antibiotics exclusion |?Sequencing type |?16S variable region |?Sequencing platform |?Data transformation |?Statistical test |?Significance threshold |?MHT correction |?LDA Score above |?Matched on |?Confounders controlled for |?Pielou |?Shannon |?Chao1 |?Simpson |?Inverse Simpson |?Richness |?State |?Reviewer" \
-  -m 'Experiment page name' \
+  -m '-' \
   -v ',' \
   -s ',' \
   -f "csv" \
@@ -27,7 +27,7 @@ echo "Done"
 echo "Exporting signatures.."
 php extensions/SemanticReports/maintenance/GenerateReport.php \
   -q "[[Category:Signatures]] |?Original page name=Signature page name |?Related experiment=Experiment |?Related study=Study |?Source data=Source |?Curated date |?Curator |?Revision editor |?Description |?Abundance in Group 1 |?NCBI_export=MetaPhlAn taxon names |?NCBI_export_ids_sc=NCBI Taxonomy IDs |?State |?Reviewer" \
-  -m 'Signature page name' \
+  -m '-' \
   -v ',' \
   -s ',' \
   -f "csv" \
